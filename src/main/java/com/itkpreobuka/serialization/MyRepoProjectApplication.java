958d738df506ebaba5e7586056fecd987cecb8da
Svetlana Zekic <ceeca1.zekic@gmail.com>
2018-12-27 20:49:46 +0100

    Add all files

---
+++ src/main/java/com/itkpreobuka/serialization/MyRepoProjectApplication.java
@@ -0,0 +1,14 @@
+package com.itkpreobuka.serialization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRepoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRepoProjectApplication.class, args);
+	}
+
+}
+
